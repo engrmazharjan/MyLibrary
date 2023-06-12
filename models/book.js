1c659d6a29137c4a0bf51469b7ba7ab0d5fffe7e
Mazhar Jan <60063589+engrmazharjan@users.noreply.github.com>
2023-06-12 15:18:22 +0500

    Adding file to database

--- models/book.js
+++ models/book.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
-const path = require("path");
 const validate = require("mongoose-validator");
-
-const coverImageBasePath = "uploads/bookCovers";
 
 const bookNameValidator = [
   validate({
@@ -43,7 +40,11 @@ const bookSchema = new mongoose.Schema({
     required: true,
     default: Date.now,
   },
-  coverImageName: {
+  coverImage: {
+    type: Buffer,
+    required: true,
+  },
+  coverImageType: {
     type: String,
     required: true,
   },
@@ -55,10 +56,11 @@ const bookSchema = new mongoose.Schema({
 });
 
 bookSchema.virtual("coverImagePath").get(function () {
-  if (this.coverImageName != null) {
-    return path.join("/", coverImageBasePath, this.coverImageName);
+  if (this.coverImage != null && this.coverImageType != null) {
+    return `data:${
+      this.coverImageType
+    };charset=utf-8;base64,${this.coverImage.toString("base64")}`;
   }
 });
 
 module.exports = mongoose.model("Book", bookSchema);
-module.exports.coverImageBasePath = coverImageBasePath;
