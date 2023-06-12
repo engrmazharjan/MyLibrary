22cbdf92641ecfcbb89d34029b52b4effd1490f2
Mazhar Jan <60063589+engrmazharjan@users.noreply.github.com>
2023-06-12 16:45:36 +0500

    Finished Authors Routes

--- models/author.js
+++ models/author.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validate = require("mongoose-validator");
+const Book = require("./book");
 
 const nameValidator = [
   validate({
@@ -15,6 +16,19 @@ const authorSchema = new mongoose.Schema({
     required: true,
     validate: nameValidator,
   },
+});
+
+authorSchema.pre("deleteOne", { document: true }, async function (next) {
+  const authorId = this._id;
+  try {
+    const bookCount = await Book.countDocuments({ author: authorId });
+    if (bookCount > 0) {
+      throw new Error("This Author Has Books Still");
+    }
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = mongoose.model("Author", authorSchema);
