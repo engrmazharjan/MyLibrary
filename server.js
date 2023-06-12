479745537a090092d7bb2b3529ad35f52512d8c4
Mazhar Jan <60063589+engrmazharjan@users.noreply.github.com>
2023-06-12 10:35:02 +0500

    Resolve mongoose error

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-// if (process.env.NODE_ENV !== "production") {
-//   require("dotenv").load();
-// }
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 const express = require("express");
-const path = require("path");
-require("dotenv").config();
-
 const app = express();
+const path = require("path");
 const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
 
@@ -17,7 +16,6 @@ app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
 
-const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
@@ -25,4 +23,8 @@ db.once("open", () => console.log("Connected to Mongoose"));
 
 app.use("/", indexRouter);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    `Server is Listening on http://localhost:${process.env.PORT || 3000}`
+  );
+});
